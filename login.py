671512d1c09def90ddcd6cd6929cd4016a88ccc7
Luiz Henrique <luizhenriquepaulinogba@gmail.com>
2023-06-29 10:18:57 -0300

    Upd

--- login.py
+++
@@ -1,7 +0,0 @@
-username = input("Username: ")
-password = input("Password: ")
-
-# Aqui seria realizado o processo de autenticação, comparando os valores inseridos com uma base de dados, por exemplo.
-# Em seguida, poderíamos redirecionar o usuário para a página principal do site.
-
-print("Login successful!")
